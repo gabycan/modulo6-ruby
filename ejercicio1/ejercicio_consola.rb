6fef54e38cd396f6bef42ffb9c563e06310be30c
maria gabriela cano portocarrero <mgabrielacano@gmail.com>
2021-09-01 18:49:24 -0400

    agregando el primer commit

---
+++ ejercicio1/ejercicio_consola.rb
@@ -0,0 +1,16 @@
+puts "¡Hola Mundo!"
+puts ("¿cómo estás?")
+
+puts "Ingresa tu nombre"
+nombre_usuario = gets.chomp
+
+puts "hola normal " + nombre_usuario
+puts "hola compuesto #{nombre_usuario}"
+
+puts "¿qué edad tienes?"
+edad = gets.chomp
+puts "#{edad} eso es mucho tiempo 💀🎂"
+
+puts "¿dónde vives?"
+lugar_vivienda = gets.chomp
+puts "¿dónde queda #{lugar_vivienda}? ❓"
